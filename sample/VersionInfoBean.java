44e293e7fc1a2e7cfb47264d74acc1e3fc277c29
Fabio Jun Takada Chino <fjtc@users.noreply.github.com>
2019-11-03 00:53:16 -0300

    Adding the sample files.

---
+++ sample/VersionInfoBean.java
@@ -0,0 +1,21 @@
+package it.is.just.a.test;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "versionBean")
+@ApplicationScoped
+public class VersionBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public VersionBean() {
+	}
+	
+	public String getVersion() {
+		return null;
+	}
+}
+
